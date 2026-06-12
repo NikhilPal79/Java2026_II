a31cf9602f50c3fdcf1b121480940f69d3c41ae6
Nikhil Pal <nikhilchpr7@gmail.com>
2026-06-11 20:05:15 -0400

    generics & fullu immutable

--- src/June09/Container.java
+++ src/June09/Container.java
@@ -1,29 +1,21 @@
 package June09;
 
-public class Container{
+public class Container<A,B>{
 
-    Student item1;
-    Course item2;
+    A Student ;
+    B CourseName;
 
 
-    public Container(Student item1, Course item2) {
-        this.item1 = item1;
-        this.item2 = item2;
-    }
-
-    public Student getItem1() {
-        return item1;
-    }
-
-    public Course getItem2() {
-        return item2;
+    public Container(A student, B courseName) {
+        Student = student;
+        CourseName = courseName;
     }
 
     @Override
     public String toString() {
         return "Container{" +
-                "item1=" + item1 +
-                ", item2=" + item2 +
+                "Student=" + Student +
+                ", CourseName=" + CourseName +
                 '}';
     }
 }
--- src/June09/ContainerMain.java
+++ src/June09/ContainerMain.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class ContainerMain {
     static void main(String[] args) {
 
-        ArrayList<Student> students = new ArrayList<>();
+        Container<Student,Course > stu = new Container<>(new Student("nikhil",5072), new  Course("Java Programming"));
+        Container<Student,Course > stu1 = new Container<>(new Student("tom",5044), new  Course("Advance Java Programming"));
+
+        System.out.println(stu);
+        System.out.println(stu1);
     }
 }
